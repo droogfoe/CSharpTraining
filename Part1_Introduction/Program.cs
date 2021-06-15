97dacaeee44f3c0606023eba5e705e6919015515
foefugl <seeddroog@gmail.com>
2021-06-15 21:05:34 +0800

    The first step.

---
+++ Part1_Introduction/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("What's your name?");
+        string UserName = Console.ReadLine();
+        Console.WriteLine("Hello {0}!", UserName);
+    }
+}
